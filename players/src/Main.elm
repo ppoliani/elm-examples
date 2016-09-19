3d74ee71f09c45a41b501cf1928203c8b3d834aa
Pavlos Polianidis <ppoliani@gmail.com>
2016-09-19 19:30:41 +0100

    setup front end project

---
+++ players/src/Main.elm
@@ -0,0 +1,24 @@
+module Main exposing (..)
+
+import Html.App
+
+import Messages exposing (Msg(..))
+import Models exposing (Model)
+import View exposing (view)
+import Update exposing (update)
+
+init: (Model, Cmd Msg)
+init = ("Hello", Cmd.none)
+
+-- Subscriptions 
+
+subscriptions: Model -> Sub Msg
+subscriptions model = Sub.none
+
+-- Main
+main: Program Never
+main = Html.App.program 
+    { init=init
+    , view=view 
+    , update=update
+    , subscriptions=subscriptions}
---
+++ players/src/Update.elm
@@ -0,0 +1,9 @@
+module Update exposing (..)
+
+import Messages exposing (Msg(..))
+import Models exposing (Model)
+
+update: Msg -> Model -> (Model, Cmd Msg)
+update msg model = 
+    case msg of 
+        NoOp -> (model, Cmd.none)
